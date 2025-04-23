4dc3e7cdb5d18c4aff02f6a0bbb0a3730f531e13
juandavid-CR7 <jdhernandezl@unah.hn>
2025-04-22 21:58:27 -0600

    cambiando puesrto de ejecucion del host

--- Backend/app.py
+++ Backend/app.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
     observer.start()
 
     try:
-        app.run(host="0.0.0.0", port=5000, debug=True)
+        app.run(host="0.0.0.0", port=5001, debug=True)
 
     finally:
         observer.stop()
